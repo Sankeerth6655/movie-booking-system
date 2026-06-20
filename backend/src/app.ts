27a95238a5f3dab4cafb667b5927d22b04338922
Sankeerth Gandla <sankeerth6655@gmail.com>
2026-06-20 17:04:17 +0530

    imporved cors origin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,10 @@ import { connectDB } from './config/db';
 import router from './routes/index.js';
 
 export const app = express();
-app.use(cors());
+app.use(cors({
+    origin:"*",
+    credentials:true
+}));
 app.use(express.json());
 
 app.use('/',router);
